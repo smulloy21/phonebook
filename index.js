6af77326b9a85a8f93bd6ad39e3f35504077b560
Shannon Mulloy <smulloy21@gmail.com>
2026-07-25 19:39:04 -0400

    validate name and phone numbers

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 
       return person.save().then((updatedPerson) => {
         response.json(updatedPerson);
-      });
+      }).catch(error => next(error));
     })
     .catch(error => next(error));
 });
@@ -66,7 +66,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -86,7 +86,7 @@ app.post('/api/persons', (request, response) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson);
-  });
+  }).catch(error => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -100,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
